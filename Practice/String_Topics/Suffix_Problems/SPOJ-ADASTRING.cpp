5bc7e86f7f137bb1611f06f8467e13cd546884a0
AppledoreM <michaelpvz@sina.com>
2018-08-02 21:41:17 +0800

    Add Problem SPOJ-ADASTRING

--- Practice/String_Topics/Suffix_Problems/SPOJ-ADASTRING.cpp
+++ Practice/String_Topics/Suffix_Problems/SPOJ-ADASTRING.cpp
@@ -10,26 +10,24 @@ bool cmp(int *r,int a,int b,int l){
 }
 
 void da(int *r,int *sa,int n,int m){
-	int i,j,p,*x = wa,*y = wb;
-	for(int i = 0; i < m; i++) wss[i] = 0;
-	for(int i = 0; i < n; i++) wss[x[i] = r[i]]++;
-	for(int i = 1; i < m; i++) wss[i] += wss[i - 1];
-	for(int i = n - 1; i >= 0; i--) sa[--wss[x[i]]] = i;
-	for(j = 1, p = 1; p < n; j *= 2, m = p){
-		for(p = 0, i = n - j; i < n; i++) y[p++] = j;
-		for(i = 0; i < n; i++) if(sa[i] >= j) y[p++] = sa[i] - j;
-		for(i = 0; i < n; i++) wv[i] = x[y[i]];
-		for(i = 0; i < m; i++) wss[i] = 0;
-		for(i = 0; i < n; i++) wss[wv[i]]++;
-		for(i = 1; i < m; i++) wss[i] += wss[i - 1];
-		for(i = n - 1; i >= 0; i--) sa[--wss[wv[i]]] = y[i];
-		swap(x,y);
-		for(p = 1,x[sa[0]] = 0,i = 1; i < n; i++){
-			x[sa[i]] = cmp(r,sa[i - 1],sa[i],j) ? p - 1 : p++;
-		}
-	}
+    int i,j,*x = wa,*y = wb,*t,p;
+    for(i = 0; i < m; i++) wss[i] = 0;
+    for(i = 0; i < n; i++) wss[x[i] = r[i]]++;
+    for(i = 1; i < m; i++) wss[i] += wss[i - 1];
+    for(i = n - 1; i >= 0; i--) sa[--wss[x[i]]] = i;
+    for(j = 1, p = 1; p < n; j *= 2, m = p){
+        for(p = 0, i = n - j; i < n; i++) y[p++] = i;
+        for(i = 0; i < n; i++) if(sa[i] >= j) y[p++] = sa[i] - j;
+        for(i = 0; i < m; i++) wss[i] = 0;
+        for(i = 0; i < n; i++) wv[i] = x[y[i]];
+        for(i = 0; i < n; i++) wss[wv[i]]++;
+        for(i = 1; i < m; i++) wss[i] += wss[i - 1];
+        for(i = n - 1; i >= 0; i--) sa[--wss[wv[i]]] = y[i];
+        for(t = x, x = y, y = t, p = 1,x[sa[0]] = 0,i = 1; i < n; i++){
+            x[sa[i]] = cmp(y,sa[i - 1],sa[i],j) ? p - 1 : p++;
+        }
+    }
 }
-
 void get_height(int *r, int n){
 	int i,j,k = 0;
 	for(int i = 1; i <= n; i++) rnk[sa[i]] = i;	
@@ -45,8 +43,7 @@ int main(){
 	}
 	da(s,sa,str.size() + 1,30);
 	get_height(s,str.size());
-	ans[str[sa[0]] - 'a'] += str.size() - sa[0];
-	for(int i = 1; i < str.size(); i++){
+	for(int i = 1; i <= str.size(); i++){
 		ans[str[sa[i]] - 'a'] += str.size() - sa[i] - height[i];
 	}
 	for(int i = 0; i < 26; i++)  cout<<ans[i]<<" ";
