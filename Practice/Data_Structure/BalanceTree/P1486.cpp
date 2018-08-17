bdff37b22c4338af882995086e6cef4d578b6926
AppledoreM <michaelpvz@sina.com>
2018-08-17 17:24:59 +0800

    Add Splay Tree Problem

--- Practice/Data_Structure/BalanceTree/P1486.cpp
+++ Practice/Data_Structure/BalanceTree/P1486.cpp
@@ -1,164 +1,153 @@
+#include "stdafx.h"
 #include <iostream>
 #include <cstring>
 #include <string>
-#include <stdio.h>
 #include <algorithm>
+#include <stdio.h>
 using namespace std;
-int N,limit;
+const int maxn = 1e6 + 10;
+int fa[maxn],cnt[maxn],ch[maxn][2],val[maxn],tag[maxn],sz[maxn];
+int root,tot,limit,N,ans;
+const int INF = -9999999;
+void maintain(int cur) {
+	sz[cur] = sz[ch[cur][0]] + sz[ch[cur][1]] + cnt[cur];
+}
+void update(int cur, int delta) {
+	if (val[cur] != INF && val[cur] != -INF) val[cur] += delta;
+	tag[cur] += delta;
+}
+void pushdown(int cur) {
+	update(ch[cur][0],tag[cur]);
+	update(ch[cur][1],tag[cur]);
+	tag[cur] = 0;
+}
+void insert(int &cur,int v) {
+	pushdown(cur);
+	if (!cur) {
+		cur = ++tot;
+		cnt[cur] = sz[cur] = 1;
+		val[cur] = v;
+	}
+	else if (v == val[cur]) {
+		cnt[cur]++;
+		sz[cur]++;
+	}
+	else if (v < val[cur]) {
+		insert(ch[cur][0], v);
+		fa[ch[cur][0]] = cur;
+		maintain(cur);
+	}
+	else {
+		insert(ch[cur][1], v);
+		fa[ch[cur][1]] = cur;
+		maintain(cur);
+	}
+}
+void clear(int cur) {
+	val[cur] = ch[cur][0] = ch[cur][1] = sz[cur] = cnt[cur] = fa[cur] = 0;
+}
+int which(int x) { return ch[fa[x]][1] == x; }
+void rotate(int x) {
+	int y = fa[x], z = fa[y];
+	pushdown(y); pushdown(x);
+	int k = which(x); int r = which(y);
+	ch[y][k] = ch[x][!k]; fa[ch[y][k]] = y;
+	ch[x][!k] = y; fa[y] = x;
+	if(z) ch[z][r] = x; fa[x] = z;
+	maintain(y); maintain(x);
+}
 
+void splay(int x, int target = 0){
+	for (; fa[x] != target; rotate(x)) {
+		pushdown(fa[x]);
+		int y = fa[x], z = fa[y];
+		if (z != target) which(x) == which(y) ? rotate(y) : rotate(x);
+	}
+	if (!target) root = x;
+}
+int find_min(int cur) {
+	pushdown(cur);
+	if (val[cur] == INF) return cur;
+	else return find_min(ch[cur][0]);
+}
+int find_val(int cur, int v) {
+	if (!cur) return 0;
+	pushdown(cur);
+	if (val[cur] == v) return cur;
+	else if (val[cur] < v) return find_val(ch[cur][1], v);
+	else return find_val(ch[cur][0], v);
+}
+int succ(int cur) {
+	splay(cur);
+	cur = ch[cur][1];
+	while (ch[cur][0]) cur = ch[cur][0];
+	return cur;
+}
+int find_k(int cur, int s) {
+	pushdown(cur);
+	if (sz[ch[cur][0]] >= s) return find_k(ch[cur][0], s);
+	else if (sz[ch[cur][0]] < s && s <= sz[ch[cur][0]] + cnt[cur]) return cur;
+	else return find_k(ch[cur][1], s - cnt[cur] - sz[ch[cur][0]]);
+}
+void dfs(int cur) {
+	if(!cur) return;
+	dfs(ch[cur][0]);
+	cout << val[cur] << " ";
+	dfs(ch[cur][1]);
+}
+void print() {
+	dfs(root);
+	puts("");
+	puts("-----------------------");
+}
+void erase() {
+	int x = find_min(root);
+	int y = find_val(root,limit - 1);
+	int add = 0;
+	if (!y) {
+		add++;
+		insert(root, limit - 1);
+		y = find_val(root, limit - 1);
+	}
+	y = succ(y);
+	splay(x);
+	splay(y, x);
+	ans += sz[ch[y][0]] - add;
+	clear(ch[y][0]);
+	ch[y][0] = 0;
+	maintain(y);
+	maintain(x);
+}
 
-class Splay{
-	public:
-		struct node{
-			node *fa,*ch[2];
-			int val,sz,cnt,tag;
-			bool relation() { return this->fa->ch[1] == this;}	
-			node(int a = 0):val(a),cnt(1),sz(1),tag(0),fa(NULL){};
-			~node(){ delete fa;}
-		}*root;
-		Splay(){ root = NULL;}
-		void pushdown(node *cur){
-			if(!cur || !cur->tag) return;
-			if(cur->ch[0]){
-				cur->ch[0]->tag += cur->tag;
-				cur->ch[0]->val += cur->tag;
-			}
-			if(cur->ch[1]){
-				cur->ch[1]->tag += cur->tag;
-				cur->ch[1]->val += cur->tag;
-			}
-			cur->tag = 0;
-		}
-		void maintain(node *cur){
-			cur->sz = cur->cnt;
-			if(cur->ch[0]) cur->sz += cur->ch[0]->sz;
-			if(cur->ch[1]) cur->sz += cur->ch[1]->sz;	
-		}
-		void rotate(node *x){
-			node* y = x->fa;
-			node* z = y->fa;
-			pushdown(y); pushdown(x);
-			x->fa = z; if(z) z->ch[y->relation()] = x;
-			int k = x->relation();
-			y->ch[k] = x->ch[!k]; if(x->ch[!k]) x->ch[!k]->fa = y;
-			x->ch[!k] = y; y->fa = x;
-			maintain(y); maintain(x);
-		}
-		void splay(node *cur,node *target = NULL){
-			while(cur->fa != target){
-				if(cur->fa->fa == target){
-					rotate(cur);
-				}
-				else if(cur->relation() == cur->fa->relation()){
-					rotate(cur->fa); rotate(cur);
-				}
-				else{
-					rotate(cur); rotate(cur);
-				}
-			}
-			if(target == NULL) root = cur;
-		}
-		node* extreme(node* cur,int k){
-			while(cur->ch[k]) cur = cur->ch[k];
-			return cur;
-		}
-		void insert(node *&cur,int val){
-			if(cur == NULL){
-				cur = new node(val);
-				return;
-			}
-			if(cur->val == val){
-				cur->cnt++;
-				cur->sz++;
-				return;
-			}
-			if(val < cur->val){
-				cur->sz++;
-				insert(cur->ch[0],val);
-				cur->ch[0]->fa = cur;
-			}
-			else {
-				cur->sz++;
-				insert(cur->ch[1],val);
-				cur->ch[1]->fa = cur;
-			}
-		}
-		node* pred(node* cur){
-			cur = cur->ch[0];
-			while(cur->ch[1]) cur = cur->ch[1];
-			return cur;
-		}
-		node* succ(node* cur){
-			cur = cur->ch[1];
-			while(cur->ch[0]) cur = cur->ch[0];
-			return cur;
-		}
-		void remove(node* cur){
-			node *pre = pred(cur), *suc = succ(cur);
-			cout<<pre<<" "<<suc<<endl;
-		/*	splay(pre); splay(suc,pre);
-			if(cur->sz > 1){
-				cur->cnt--;
-				cur->sz--;
-			}
-			else{
-				delete suc->ch[0];
-				suc->ch[0] = NULL;
-			}
-			maintain(suc);
-			maintain(pre);
-			*/
-		}
-		void print(node* cur){
-			if(cur == NULL) return;
-			pushdown(cur);
-			print(cur->ch[0]);
-			cout<<cur->val<<" ";
-			print(cur->ch[1]);
-		}
-		node* find_val(node* cur,int val){
-			if(!cur) return NULL;
-			if(val < cur->val) return find_val(cur->ch[0],val);
-			else if(val == cur->val) return cur;
-			else return find_val(cur->ch[1],val);	
-		}
-		node* first_great(int val){
-			node* p = root, *p1 = root;
-			while(p){
-				if(p->val < val) p = p->ch[1];
-				else p1 = p, p = p->ch[0];
-			}
-			if(p1->val < limit) {root = NULL; return NULL;}
-			return p1;
-		}
-};
-int main(){
+int main() {
 	ios::sync_with_stdio(false);
-	Splay *sp = new Splay();
-	cin>>N>>limit;
-	sp->insert(sp->root,-99999);
-	for(int i = 1; i <= N; i++){
+	cin >> N >> limit;
+	insert(root, INF); insert(root, -INF);
+	for (int i = 1; i <= N; i++) {
 		char opt;
-		int val;
-		cin>>opt>>val;
-		if(opt == 'I'){
-			if(val >= limit) sp->insert(sp->root,val);
+		int v;
+		cin >> opt >> v;
+		if (opt == 'I') {
+			if (v < limit) continue;
+			insert(root, v);
 		}
 		else if(opt == 'A'){
-			sp->root->tag += val;
-			sp->root->val += val;	
+			update(root, v);
 		}
-		else if(opt == 'S'){
-			sp->root->tag -= val;
-			sp->root->val -= val;
-			sp->print(sp->root);
-			sp->splay(sp->extreme(sp->root,0));
-			Splay::node* oth = sp->first_great(limit);
-			if(oth == NULL) continue;
-			sp->splay(oth,sp->root);
-			sp->remove(sp->root->ch[1]->ch[0]);
+		else if (opt == 'S') {
+			update(root, -v);
+			erase();
 		}
-		else sp->print(sp->root);
+		else {
+			if (v < 1 || v > sz[root] - 2) cout << -1 << endl;
+			else {
+				int re = val[find_k(root, sz[root] - v)];
+				if (re == INF || re == -INF) cout << -1 << endl; 
+				else cout << re << endl;
+			}
+		}
+//		print();
 	}
+	cout << ans << endl;
 	return 0;
 }
